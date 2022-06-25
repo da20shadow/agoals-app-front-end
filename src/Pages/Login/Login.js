f24d106321a5edcd7426b3a612a43048c11ba994
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-06-25 19:23:53 +0300

    Login page style

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,85 @@
-function Login(){
-
-    const processLogin = (e) => {
+import './scss/login.css';
+import axios from "axios";
+import {BsCheckLg, BsFillExclamationTriangleFill} from "react-icons/bs";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import * as authService from '../../Services/AuthService';
+function Login() {
+    const redirect = useNavigate();
+    const [notification, setNotification] = useState();
+    const processLogin = async (e) => {
         e.preventDefault();
-        console.log('Login Clicked!')
+        const formData = new FormData(e.currentTarget);
+        const {username, password} = Object.fromEntries(formData);
+
+        let responseMessage = authService.login({username,password});
+
+        console.log(responseMessage)
+
+        const message = (
+            <div className="alert alert-success-flex align-items-center
+                        alert-success fade show position-absolute alert-position"
+                            role="alert">
+            <BsCheckLg size={'24px'} className={'bi flex-shrink-0 me-2'}/>
+            <strong> Successfully logged in! </strong>
+            </div>
+        );
+
+        const error = (
+            <div className="alert alert-danger-flex align-items-center
+                        alert-danger fade show position-absolute alert-position"
+                 role="alert">
+                <BsFillExclamationTriangleFill size={'24px'} className={'bi flex-shrink-0 me-2'} />
+                <strong> Error: </strong> {err.response.data.message}
+            </div>
+        );
+
+        // setNotification(successMsg);
+        setTimeout(()=>{
+            // redirect('/account');
+        },2500)
+
+        if (!notification){
+            setTimeout(()=>{
+                setNotification('');
+            },3000);
+        }
     }
 
     return (
-        <form className={'loginForm'} onSubmit={processLogin}>
-            <input name={'username'} type="text" placeholder={'Username'}/>
+        <div className={'container'}>
 
-            <input name={'password'} type="password" placeholder={'Password'} />
+            <div className="row">
 
-            <input type={'submit'} value={'Login'}/>
-        </form>
+                <div id="message" className="text-center">{notification}</div>
+
+                <form method="post" className={'loginForm'} onSubmit={processLogin}>
+
+                    <h1 className="text-center">Login</h1>
+
+                    <div className="my-3">
+                        <input type="text"
+                               name={'username'}
+                               className=" invalidInput"
+                               placeholder={'Username'}
+                               required/>
+                    </div>
+
+                    <div className="mb-3">
+                        <input type="password"
+                               name={'password'}
+                               className=" is-invalid"
+                               placeholder={'Password'}
+                               required/>
+                    </div>
+
+                    <div className="mb-3">
+                        <button>Login</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
     )
 }
 
