eb129ad0b040db38aabe0b274b05d4cd67c05d9b
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-06-27 20:14:57 +0300

    Routes redirection when not logged in

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import {Routes, Route} from 'react-router-dom';
-import Home from "./Pages/Home";
-import Header from "./Components/Header";
-import Tasks from "./Pages/Tasks";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
+import './Assets/Scss/App.css';
+import {Routes, Route, Navigate } from 'react-router-dom';
+import {Header, Footer, Sidebar} from "./Components";
+import {Home, Login, Register, Account, Tasks, Goals, CreateGoal} from "./Pages";
+import {useStateContext} from "./Contexts/ContextProvider";
+import {useAuthContext} from "./Contexts/AuthContext";
 
 function App() {
-
+    const {isLogged} = useAuthContext();
+    const {activeMenu} = useStateContext();
     return (
         <>
-            <Header />
-            <Routes>
-                <Route path={'/'} element={<Home />}/>
-                <Route path={'/login'} element={ <Login /> }/>
-                <Route path={'/register'} element={ <Register/> }/>
-                <Route path={'/tasks'} element={ <Tasks /> }/>
-            </Routes>
+            <div className={'flex relative dark:bg-main-dark-bg'}>
+
+                {activeMenu ? (
+                    <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+                        <Sidebar/>
+                    </div>
+                ) : (
+                    <div className="w-0 dark:bg-secondary-dark-bg">
+                        <Sidebar/>
+                    </div>
+                )}
+                <div className={`dark:bg-main-bg  bg-main-bg min-h-screen w-full
+                                    ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+
+                    <div className={'fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'}>
+
+                        <Header/>
+
+                    </div>
+
+                    <Routes>
+                        <Route path={'/'} element={<Home/>}/>
+                        <Route path={'/login'} element={isLogged ? <Account/> : <Login/>}/>
+                        <Route path={'/register'} element={isLogged ? <Account/> : <Register/>}/>
+                        <Route path={'/account'} element={isLogged ? <Account/> : <Navigate to={'/login'}/> }/>
+                        <Route path={'/create-goal'} element={isLogged ? <CreateGoal/> : <Navigate to={'/login'}/>}/>
+                        <Route path={'/goals'} element={isLogged ? <Goals/> : <Navigate to={'/login'}/>}/>
+                        <Route path={'/tasks'} element={isLogged ? <Tasks/> : <Navigate to={'/login'}/>}/>
+                    </Routes>
+                </div>
+
+            </div>
+
+            <Footer/>
+
 
         </>
     );
