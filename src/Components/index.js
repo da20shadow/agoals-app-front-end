03b5c167c37c7fb9ce854665bedbf0a41403322c
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-06-27 20:23:00 +0300

    added index file to export the components at once

---
+++ src/Components/index.js
@@ -0,0 +1,10 @@
+export {default as Header} from './Header';
+export {default as Footer} from './Footer';
+export {default as Sidebar} from './Sidebar';
+export {default as LeftNavbar} from './LeftNavbar';
+export {default as Button} from './Button';
+export {default as Notification} from './Notification';
+export {default as UserProfile} from './UserProfile';
+export {default as ChartHeader} from './Charts/ChartHeader';
+export {default as Pie} from './Charts/Pie';
+export {default as Line} from './Charts/Line';
