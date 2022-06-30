ae7cf2d0c150467a1c8bc73d0e197ef31fbf85e9
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-06-30 20:56:07 +0300

    Removed function

--- src/Contexts/ContextProvider.js
+++ src/Contexts/ContextProvider.js
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, {createContext, useContext, useState} from 'react';
+import * as goalService from "../Services/GoalService";
 
 const StateContext = createContext();
 
@@ -9,23 +10,49 @@ const initialState = {
     notification: false,
 };
 
-export const ContextProvider = ({ children }) => {
+export const ContextProvider = ({children}) => {
 
-    const [screenSize,setScreenSize] = useState(initialState);
-    const [activeMenu,setActiveMenu] = useState(false);
-    const [activeUserProfileMenu,setUserProfileMenu] = useState(false);
+    const [screenSize, setScreenSize] = useState(initialState);
+    const [activeMenu, setActiveMenu] = useState(false);
+    const [activeUserProfileMenu, setUserProfileMenu] = useState(false);
+    const [goals, setGoals] = useState([]);
 
-    return (
-        <StateContext.Provider
-            value={{
-                activeMenu, setActiveMenu,
-                activeUserProfileMenu, setUserProfileMenu,
-                screenSize,setScreenSize,
-            }}
-        >
-            {children}
-        </StateContext.Provider>
-    )
+    const getAllGoals = async (user_id) => {
+        if (goals.length === 0) {
+            //TODO: Remove the console logs
+            console.log('ContextProvider.js Server Request For All Goals Making Now!')
+            goalService.getAll(user_id)
+                .then(res => {
+                    setGoals(res.goals)
+                })
+                .catch(err => {
+                    console.log('ContextProvider getAllGoals() Fetch Error: ', err)
+                })
+        }
+    }
+
+    const updateAllGoals = async (user_id) => {
+        goalService.getAll(user_id)
+            .then(res => {
+                setGoals(res.goals)
+            })
+            .catch(err => {
+                console.log('ContextProvider getAllGoals() Fetch Error: ', err)
+            })
+    }
+
+return (
+    <StateContext.Provider
+        value={{
+            activeMenu, setActiveMenu,
+            activeUserProfileMenu, setUserProfileMenu,
+            screenSize, setScreenSize,
+            goals, setGoals, getAllGoals,updateAllGoals
+        }}
+    >
+        {children}
+    </StateContext.Provider>
+)
 }
 
 export const useStateContext = () => useContext(StateContext);
