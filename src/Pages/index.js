113c5d7af11e8ec1a603122f0042d14365e30863
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-06-30 20:56:44 +0300

    Created index for faster and easy Pages export

---
+++ src/Pages/index.js
@@ -0,0 +1,8 @@
+export {default as Home} from './Home';
+export {default as Login} from './Login';
+export {default as Register} from './Register';
+export {default as Account} from './Account';
+export {default as Goals} from './Goals';
+export {default as SingleGoal} from './SingleGoal';
+export {default as CreateGoal} from './CreateGoal';
+export {default as Tasks} from './Tasks';
