43fb86c19e84e8a82b5680ac64826dbb1383b73d
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-01 18:08:54 +0300

    Task Due Date component

--- src/Pages/SingleGoal/Components/Task/Components/TaskDueDate/TaskDueDate.js
+++ src/Pages/SingleGoal/Components/Task/Components/TaskDueDate/TaskDueDate.js
@@ -1,24 +1,54 @@
+import {dateFormat} from "../../../../../../Services/GoalService";
+import {BsCalendarPlus} from "react-icons/bs";
+import {updateTaskForGoalId} from "../../../../../../Services/TaskService";
+import {useAuthContext} from "../../../../../../Contexts/AuthContext";
+
 function TaskDueDate (
     {
+        taskId,
+        setUpdateTasksList,
         dueDate,
         showHideDate,
         setShowHideDate
     }
 ){
+    const due_date = dateFormat(dueDate);
+    const {user} = useAuthContext();
+
+    const changeDueDate = (e) => {
+        setShowHideDate('hidden');
+
+        const data = {
+            user_id: user.user_id,
+            token: user.token,
+            due_date: e.currentTarget.value
+        }
+
+        updateTaskForGoalId(taskId,data)
+            .then(res => {
+                console.log('Success Date Changed: ', res)
+                setUpdateTasksList(true);
+            })
+            .catch(err => {
+                console.log('Error Date not changed: ',err)
+                console.log('Error Date not changed: ',err.response.data)
+            })
+    }
+
     return(
         <div className={'col-span-2 flex justify-center'}>
             {showHideDate === 'hidden'
                 ? (
                     <span className={'text-sm cursor-pointer'}
                           onClick={() => setShowHideDate('block')}>
-                                                    {dueDate ? dueDate : 'Loading..'}
+                                                    {due_date ? due_date : <BsCalendarPlus />}
                                                 </span>
                 )
                 : (
                     <input className={'text-sm cursor-pointer'}
                            type="date"
-                           value={dueDate ? dueDate : 'Loading..'}
-                           onChange={() => setShowHideDate('hidden')}
+                           value={due_date && due_date}
+                           onChange={changeDueDate}
                     />
                 )
             }
