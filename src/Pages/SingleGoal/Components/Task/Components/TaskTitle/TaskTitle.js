1a7553c91eea552d8d260c28f53af5899c631991
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-01 18:14:15 +0300

    Added function for changing dinamicaly the Task title

--- src/Pages/SingleGoal/Components/Task/Components/TaskTitle/TaskTitle.js
+++ src/Pages/SingleGoal/Components/Task/Components/TaskTitle/TaskTitle.js
@@ -1,10 +1,40 @@
+import {useAuthContext} from "../../../../../../Contexts/AuthContext";
+import {updateTaskForGoalId} from "../../../../../../Services/TaskService";
+import {useState} from "react";
+
 function TaskTitle(
     {
-        title,
+        taskId,
+        task_title,
         showHideTitle,
         setShowHideTitle
     }
 ){
+    const {user} = useAuthContext();
+    const [title,setTitle] = useState(task_title);
+
+    const changeTitle = (e) => {
+        setShowHideTitle('hidden');
+        console.log('Input: ',e.currentTarget.value)
+        const changedTitle = e.currentTarget.value;
+        //TODO Validate the input!
+        const data = {
+            user_id: user.user_id,
+            token: user.token,
+            title: changedTitle
+        }
+
+        updateTaskForGoalId(taskId,data)
+            .then(res => {
+                console.log('Success Title Changed: ', res)
+                setTitle(changedTitle);
+            })
+            .catch(err => {
+                console.log('Error Title not changed: ',err)
+                console.log('Error Title not changed: ',err.response.data)
+            })
+    }
+
     return(
         <div className={'col-span-3 flex justify-center'}>
             {showHideTitle === 'hidden'
@@ -18,7 +48,7 @@ function TaskTitle(
                     <input className={'w-full cursor-pointer italic border-2 text-blue border-blue bg-blue-bg '}
                            type="text"
                            defaultValue={title ? title : 'Loading..'}
-                           onBlur={() => setShowHideTitle('hidden')}
+                           onBlur={changeTitle}
                     />
                 )
             }
