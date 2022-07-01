75d8660521e8f3fabca2b4efc714a015a99e40e4
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-01 18:08:21 +0300

    Task Status Component

--- src/Pages/SingleGoal/Components/Task/Components/Status/Status.js
+++ src/Pages/SingleGoal/Components/Task/Components/Status/Status.js
@@ -2,33 +2,61 @@ import {DropDownMenu} from "../../../../../../Components";
 import {GiSandsOfTime} from "react-icons/gi";
 import {FaEdit} from "react-icons/fa";
 import {BsCheckLg} from "react-icons/bs";
+import {useAuthContext} from "../../../../../../Contexts/AuthContext";
+import {updateTaskForGoalId} from "../../../../../../Services/TaskService";
 
 function Status(
     {
+        taskId,
+        setUpdateTasksList,
         taskStatus,
         setTaskStatus,
         showHideStatus,
         setShowHideStatus,
     }
 ){
+    const {user} = useAuthContext();
 
     const changeStatus = (e) => {
         let status;
+        let statusNumber;
         switch (e.currentTarget.textContent) {
-            case 'To Do':
-                status = <span className={'rounded-md text-primary bg-light-gray py-2 px-3'}>To Do</span>;
-                break;
             case 'In Progress':
+                statusNumber = 1;
                 status = <span className={'rounded-md text-red bg-red-bg py-2 px-3'}> In Progress</span>;
                 break;
             case 'In Review':
+                statusNumber = 2;
                 status = <span className={'rounded-md text-blue bg-blue-bg py-2 px-3'}> In Review</span>;
                 break;
+            case 'To Do':
+                statusNumber = 3;
+                status = <span className={'rounded-md text-primary bg-light-gray py-2 px-3'}>To Do</span>;
+                break;
             case 'Completed':
+                statusNumber = 4;
                 status = <span className={'rounded-md text-green bg-green-bg py-2 px-3'}> Completed</span>;
                 break;
+            default:
+                statusNumber = 3;
+                status = <span className={'rounded-md text-primary bg-light-gray py-2 px-3'}>To Do</span>;
         }
-        setTaskStatus(status);
+        const data = {
+            user_id: user.user_id,
+            token: user.token,
+            status: statusNumber
+        }
+        updateTaskForGoalId(taskId,data)
+            .then(res => {
+                console.log('Success Task Status Changed: ', res)
+                setTaskStatus(status);
+                setUpdateTasksList(true);
+            })
+            .catch(err => {
+                console.log('Error Status not changed: ',err)
+                console.log('Error Status not changed: ',err.response.data)
+            })
+
     }
 
     return(
